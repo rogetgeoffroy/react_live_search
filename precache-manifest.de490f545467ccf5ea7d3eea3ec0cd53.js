5286b8086d9eff515da83f8a1175ed39eca6733d
rogetgeoffroy26 <rogetgeoffroy@gmail.com>
2020-12-07 10:30:26 -0500

    Updates

---
+++ precache-manifest.de490f545467ccf5ea7d3eea3ec0cd53.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82776fbb2591824926d9efdadd15bdfc",
+    "url": "/react_live_search/index.html"
+  },
+  {
+    "revision": "78d09fa9db6b59c783ba",
+    "url": "/react_live_search/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "07ae51d41541cbd70f23",
+    "url": "/react_live_search/static/css/main.0212b912.chunk.css"
+  },
+  {
+    "revision": "78d09fa9db6b59c783ba",
+    "url": "/react_live_search/static/js/2.e545cf30.chunk.js"
+  },
+  {
+    "revision": "50c3cbab39dddffbc7bac710afc34c8f",
+    "url": "/react_live_search/static/js/2.e545cf30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ae51d41541cbd70f23",
+    "url": "/react_live_search/static/js/main.44694291.chunk.js"
+  },
+  {
+    "revision": "a704da9fc37b7a526346",
+    "url": "/react_live_search/static/js/runtime-main.dd9303dc.js"
+  },
+  {
+    "revision": "f0aa89d24b4264a10241a491edc7cb08",
+    "url": "/react_live_search/static/media/Hero.f0aa89d2.jpg"
+  },
+  {
+    "revision": "c2cef10366081e6931aec5f020f70b9a",
+    "url": "/react_live_search/static/media/Main_Logo.c2cef103.svg"
+  },
+  {
+    "revision": "ea9d0115a76afda1ca7e9b8bc0b3690f",
+    "url": "/react_live_search/static/media/arrows-alt-h-solid.ea9d0115.svg"
+  },
+  {
+    "revision": "4f314ccd327ee4d8694c5507e2e6c99f",
+    "url": "/react_live_search/static/media/hand-pointer-solid.4f314ccd.svg"
+  },
+  {
+    "revision": "de50e2e9ca0fc52b57dbe9600b542e19",
+    "url": "/react_live_search/static/media/loader.de50e2e9.gif"
+  }
+]);
